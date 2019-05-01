bb38a2859780e52e750960786780c788d0285a29
Baruch David <ba.da@yahoo.de>
2019-05-01 12:38:22 +0200

    initial redit clone commit

---
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/RkaRedditApplication.java
@@ -0,0 +1,13 @@
+package de.ffm.rka.rkareddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkaRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkaRedditApplication.class, args);
+	}
+
+}
