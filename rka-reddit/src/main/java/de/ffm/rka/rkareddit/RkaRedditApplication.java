1db9446abfe849de5cfaa587ee90e99003fde45f
KAPTSAN <roman.kaptsan@lht.dlh.de>
2019-05-02 10:49:38 +0200

    add lib for own config classes, add logger, manage profile on application.properties

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/RkaRedditApplication.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/RkaRedditApplication.java
@@ -1,12 +1,18 @@
 package de.ffm.rka.rkareddit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import net.bytebuddy.asm.Advice.This;
 
 @SpringBootApplication
 public class RkaRedditApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(RkaRedditApplication.class);
 	public static void main(String[] args) {
+		LOGGER.info("Info Message");
 		SpringApplication.run(RkaRedditApplication.class, args);
 	}
 
