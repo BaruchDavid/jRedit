866b240659de418842478e65347882130a425620
Baruch David <ba.da@yahoo.de>
2019-05-12 13:17:43 +0200

    add static resources

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/HomeController.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/HomeController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/")
-	public String home(Model model) {
-		model.addAttribute("message", "Hello Redit");
-		return "index";
-	}
+//	@GetMapping("/")
+//	public String home(Model model) {
+//		model.addAttribute("message", "Hello Redit");
+//		return "index";
+//	}
 }
