b118f7051e67dad233109c9d7df253b4505e57b6
Baruch David <ba.da@yahoo.de>
2019-05-21 20:16:05 +0200

    upvote, downvote, complete auditable fields

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/config/AuditorAwareImpl.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/config/AuditorAwareImpl.java
@@ -7,21 +7,29 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import de.ffm.rka.rkareddit.domain.User;
+import de.ffm.rka.rkareddit.security.UserDetailsServiceImpl;
 
 
 public class AuditorAwareImpl implements AuditorAware<String>{
 
+
+	
 	/**
 	 * will be invoked for auditing, when trying to save anythng 
 	 */
 	@Override
 	public Optional<String> getCurrentAuditor() {
-		Optional<String> userName = Optional.of(((User)SecurityContextHolder.getContext()
-				 .getAuthentication()
-				 .getPrincipal()).getEmail());
+		Optional<String> userName = Optional.empty();
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+		   userName = Optional.ofNullable(authentication.getName());
+		}
 		return userName;
 	}
 }
--- rka-reddit/src/main/resources/static/js/vote.js
+++ rka-reddit/src/main/resources/static/js/vote.js
@@ -1,16 +1,17 @@
 $('.upvote, .downvote').on('click', function(){
             const direction = this.className==='upvote' ? 1 : -1;
             const linkId = this.getAttribute('data-id');
-            const voteCount = $('#votecount-'+this.getAttribute("data-id")).text();
+            const voteCount = $('#votecount-'+this.getAttribute("data-id"));
+           
             
             
-            fetch(`/jReditt/vote/link/${linkId}/direction/${direction}/votecount/${voteCount}`)
-                    .then(response => response.json())
-                    .then(data => voteSum.innerHTML = data)
-                    .catch(err => console.error(err))            
-            // fetch(`/jReditt/links/vote/link/${linkId}/direction/${direction}/votecount/${voteCount}`)
-            //         .then(response => response.json())
-            //         .then(data => voteSum.innerHTML = data)
-            //         .catch(err => console.error(err))
-
+            fetch(`/jReditt/vote/link/${linkId}/direction/${direction}/votecount/${voteCount.text()}`)
+                    .then(
+                        response => response.json())
+                    .then(data => 
+                            voteCount.text(data)
+                        )
+                    .catch(
+                        err => console.error(err)
+                    );        
         });
