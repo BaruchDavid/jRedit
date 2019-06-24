862488295caa7f0598679946295201c1c6ac8af2
Kaptsan <roman.kaptsan@cgi.com>
2019-06-24 12:44:24 +0200

    remove unused import

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/config/AuditorAwareImpl.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/config/AuditorAwareImpl.java
@@ -3,18 +3,14 @@ package de.ffm.rka.rkareddit.config;
 
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import de.ffm.rka.rkareddit.domain.User;
-import de.ffm.rka.rkareddit.security.UserDetailsServiceImpl;
+
 
 
 public class AuditorAwareImpl implements AuditorAware<String>{
