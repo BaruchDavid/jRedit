916fc8611a1f5683ea78423436d6907b18ddfee9
kaproma <kaproma@yahoo.de>
2019-09-18 12:58:28 +0200

    On branch external_tomcat Changes to be committed: 	modified:   rka-reddit/pom.xml 		    package as war file for external tomcat 		    add tomcat-server-starter for external tomcat 	modified:   rka-reddit/src/main/java/de/ffm/rka/rkareddit/RkaRedditApplication.java 		    implement Servlet super-class on startpoint for external tomcat

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/RkaRedditApplication.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/RkaRedditApplication.java
@@ -5,18 +5,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
-public class RkaRedditApplication {
+public class RkaRedditApplication extends SpringBootServletInitializer {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RkaRedditApplication.class);
 	public static void main(String[] args) {
 		LOGGER.info("START JREDITT APPLICATION");
 		SpringApplication.run(RkaRedditApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(RkaRedditApplication.class);
+	}
+	
+	
 	
 }
