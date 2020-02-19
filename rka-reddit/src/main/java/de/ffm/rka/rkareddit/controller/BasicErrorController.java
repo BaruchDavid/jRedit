a00f101a7ea9b07852b9b9f85e7daa4908594dfa
Kaptsan <roman.kaptsan@cgi.com>
2020-02-19 20:10:36 +0100

    remove not used autocomplite.js, simplefy ErrorController

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/BasicErrorController.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/BasicErrorController.java
@@ -3,7 +3,8 @@ package de.ffm.rka.rkareddit.controller;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BasicErrorController implements ErrorController{
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(BasicErrorController.class);
+	
 	@Override
 	public String getErrorPath() {
 		// TODO Auto-generated method stub
@@ -21,16 +23,7 @@ public class BasicErrorController implements ErrorController{
 	@RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-	     
-	    if (status != null) {
-	        Integer statusCode = Integer.valueOf(status.toString());
-	     
-	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	            return "error-404";
-	        } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	            return "error-500";
-	        }
-	    }
+		LOGGER.error("STATUS-CODE {} beim Aufruf folgendes Requests {}", status, request.getRequestURI());
         return "error/basicError";
     }
 
