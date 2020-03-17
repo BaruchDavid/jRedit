eff1f25983aefa66f35127a6a143316c9de5235d
Kaptsan <roman.kaptsan@cgi.com>
2020-03-17 15:36:42 +0100

    set each tag on own input-field

--- rka-reddit/src/main/resources/static/js/tag.js
+++ rka-reddit/src/main/resources/static/js/tag.js
@@ -1,28 +1,29 @@
-$('#tag').keyup(function(event){
-	if(event.keyCode === 32){
-		var tags = $('#tag').val().trim().split(' ');
-		var tag = tags[tags.length-1];
+$('.tag-input').focusout(function(event){
+//	if(event.keyCode === 32){
+		var tag = event.target;
 		$.ajax({
 			url : "/jReditt/tags/tag/create",
 			type : 'post',
 			dataType : "text",
-			data : tag,
+			data : tag.value,
 			success : function(data) {
 				var id = JSON.parse(data).tagId;
 				var name = JSON.parse(data).name;
-				for(var i=0; i<4; i++){
-					if(document.getElementById("tags"+i+".tagId")!==null){
-						if(document.getElementById("tags"+i+".tagId").value === '0'){
-							document.getElementById("tags"+i+".tagId").value = id;
-							document.getElementById("tags"+i+".name").value = name;
-							break;
-						}
-					}
-				}
+				document.getElementById(tag.id+".tagId").value = id;
+				document.getElementById(tag.id+".name").value = name;
 			},
 			error: function (request, status, error) {
 		        console.log(request.responseText);
 		    }
 		});
-	}
+//	} else if(event.keyCode === 8){
+		//1. java java8 spring
+		//2. diese drei Worte kommen in die Tabelle drunter
+		//3. man entfernt java8
+		//5. Man holt sichübrigt gebliebene Wörter: currentValues = $('tag').val().sprlit(' ');
+		//5. Bei jedem Entfernen, durchsucht man die Tabelle, ob man das zu entfernende Wort auch aus der Tabelle löschen kann
+		//8. In der Tabelle übrig gebliebene Wörter werden übertragen
+		
+//	}
+
 });
