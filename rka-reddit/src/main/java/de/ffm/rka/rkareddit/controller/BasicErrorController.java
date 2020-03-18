9250d58637d5cd1541b3c4c6411a2d2d14e710fe
Kaptsan <roman.kaptsan@cgi.com>
2020-03-18 20:53:04 +0100

    add user to model due of lost user on error within main_layout

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/BasicErrorController.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/BasicErrorController.java
@@ -1,29 +1,41 @@
 package de.ffm.rka.rkareddit.controller;
 
+import java.util.Optional;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import de.ffm.rka.rkareddit.domain.User;
+import de.ffm.rka.rkareddit.security.UserDetailsServiceImpl;
 
 @Controller
 public class BasicErrorController implements ErrorController{
 	private static final Logger LOGGER = LoggerFactory.getLogger(BasicErrorController.class);
+	@Autowired
+	UserDetailsServiceImpl userDetailsService;
 	
 	@Override
 	public String getErrorPath() {
-		// TODO Auto-generated method stub
 		return "/error";
 	}
 	
 	@RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-		LOGGER.error("STATUS-CODE {} beim Aufruf folgendes Requests {}", status, request.getRequestURI());
+		Optional<Authentication> authetication = Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication());
+		User user = (User) userDetailsService.loadUserByUsername(authetication.get().getName());
+		model.addAttribute("user", user);
         return "error/basicError";
     }
 
