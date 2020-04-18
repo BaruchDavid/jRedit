a8d5311d68208315155742a6deed2aa11f0c2e46
Kaptsan <roman.kaptsan@cgi.com>
2020-04-18 17:27:31 +0200

    remove homecontroller, move function to httpUtil

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/util/HttpUtil.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/util/HttpUtil.java
@@ -1,10 +1,11 @@
-package de.ffm.rka.rkareddit.controller;
-
+package de.ffm.rka.rkareddit.util;
 
 import java.util.Enumeration;
 import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
@@ -12,37 +13,25 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+@Service
+public class HttpUtil {
 
-@Controller
-@RequestMapping("/")
-public class HomeController {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
-
-	/**
-    * @deprecated after login, request redirect to LinkController
-    */
-	@Deprecated(since = "in the moment not used")
-	@GetMapping({"/",""})
-	public String list(HttpServletRequest request, 
-						@PageableDefault(size = 6, direction = Sort.Direction.DESC, sort = "linkId") Pageable page,
-						@AuthenticationPrincipal UserDetails user, Model model) {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HttpUtil.class);
+	
+	public static void listSessionAttributesFromRequest(HttpServletRequest request) {
 		Enumeration<String> sessionAttributeNames = request.getSession().getAttributeNames();
 		HttpSession session = request.getSession();
-		while(sessionAttributeNames.hasMoreElements()) {
+		while (sessionAttributeNames.hasMoreElements()) {
 			Optional<String> attributeName = Optional.of(sessionAttributeNames.nextElement());
 			attributeName.ifPresent(attr -> {
 				LOGGER.debug("Session attribute name: {}", attr);
 				Optional<Object> attribute = Optional.of(session.getAttribute(attr));
-				LOGGER.debug("Session attribute value: {}", attribute.isPresent()?attribute.get():"no value");
+				LOGGER.debug("Session attribute value: {}", attribute.isPresent() ? attribute.get() : "no value");
 			});
 		}
-		return "link/link_list";
-		 
 	}
+
 }
