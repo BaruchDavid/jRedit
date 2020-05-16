6eda9713d3b077faed99696860930b471b5af1e5
Kaptsan <roman.kaptsan@cgi.com>
2020-05-16 23:59:20 +0200

    change user during test from userDetails.User to User from model

--- rka-reddit/src/test/java/de/ffm/rka/rkareddit/security/mock/SpringSecurityTestConfig.java
+++ rka-reddit/src/test/java/de/ffm/rka/rkareddit/security/mock/SpringSecurityTestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import de.ffm.rka.rkareddit.domain.User;
 import de.ffm.rka.rkareddit.repository.UserRepository;
+import de.ffm.rka.rkareddit.security.UserDetailsServiceImpl;
 import edu.emory.mathcs.backport.java.util.Collections;
 
 /**
@@ -25,6 +26,9 @@ public class SpringSecurityTestConfig   {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
 	@SuppressWarnings("unchecked")
 	private Collection<UserDetails> users = Collections.emptyList();
 	
@@ -35,8 +39,7 @@ public class SpringSecurityTestConfig   {
     	User masterUser = userRepository.findByEmailWithRoles("romakapt@gmx.de");
     	User user = userRepository.findByEmailWithRoles("dascha@gmx.de");
         users = Arrays.asList(masterUser, user);
-    	InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager(users);
-       	return inMemoryUserDetailsManager;
+    	return userDetailsService;
     }
 
 	public Collection<UserDetails> getUsers() {
