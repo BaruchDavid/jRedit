b5e95259ca796ee3ba0fb0430c0d7f2d1a74377c
kaproma <kaproma@yahoo.de>
2020-10-11 13:30:21 +0200

    handle votes over VoteController

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/rest/controller/VoteController.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/rest/controller/VoteController.java
@@ -1,27 +1,25 @@
 package de.ffm.rka.rkareddit.rest.controller;
 
-import de.ffm.rka.rkareddit.domain.Link;
-import de.ffm.rka.rkareddit.domain.Vote;
-import de.ffm.rka.rkareddit.domain.dto.LinkDTO;
 import de.ffm.rka.rkareddit.exception.ServiceException;
-import de.ffm.rka.rkareddit.service.LinkService;
 import de.ffm.rka.rkareddit.service.VoteService;
 import org.apache.commons.httpclient.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Optional;
+
 
 @RestController
 public class VoteController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(VoteController.class);
-
+	public static final String ANONYMOUS = "anonymous";
 	private final VoteService voteService;
 
 	public VoteController(VoteService voteService) {
@@ -30,25 +28,32 @@ public class VoteController {
 
 
 	/**
-	 * 
 	 * @param lSig which will be voted
 	 * @param direction down or top
 	 * @param voteCount is sum of votes
 	 * @return new sum of votes
-	 * @author Roman
+	 * @author RKA
 	 */
-	@GetMapping("/vote/link/{lSig}/direction/{direction}/votecount/{voteCount}")
+	@GetMapping("/link/{lSig}/vote/direction/{direction}/votecount/{voteCount}")
 	public int vote(@PathVariable String  lSig,
-					@PathVariable short direction, 
+					@PathVariable short direction,
+					@AuthenticationPrincipal UserDetails userDetails,
 					@PathVariable int voteCount, HttpServletRequest req, HttpServletResponse res) throws ServiceException {
 
-		Vote vote = voteService.saveVote(direction, lSig, voteCount);
-		if (vote.getVoteId() != null) {
-			return vote.getLink().getVoteCount();
+		if(userDetails != null && !ANONYMOUS.equals(userDetails.getUsername())){
+			int newCount = voteService.saveVote(direction, lSig, voteCount);
+			if (voteCount != newCount) {
+				return newCount;
+			} else {
+				res.setStatus(HttpStatus.SC_BAD_REQUEST);
+				LOGGER.warn("VOTE LOST FOR LINK-SIG {} FROM USER {}", lSig, req.getUserPrincipal());
+				return voteCount;
+			}
 		} else {
-			res.setStatus(HttpStatus.SC_BAD_REQUEST);
+			res.setStatus(HttpStatus.SC_UNAUTHORIZED);
 			LOGGER.warn("VOTE LOST FOR LINK-SIG {} FROM USER {}", lSig, req.getUserPrincipal());
 			return voteCount;
 		}
+
 	}
 }
