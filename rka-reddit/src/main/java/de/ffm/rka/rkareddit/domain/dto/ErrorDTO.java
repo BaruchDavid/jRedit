1fe519f290507db851c700d8da3ca17b88075808
kaproma <kaproma@yahoo.de>
2021-03-14 14:20:31 +0100

    add error-message, set default-values for builder

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/domain/dto/ErrorDTO.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/domain/dto/ErrorDTO.java
@@ -1,14 +1,26 @@
 package de.ffm.rka.rkareddit.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorDTO {
+
     UserDTO loggedUser;
     UserDTO userContent;
-    String url;
-    String errorView;
+    @Builder.Default
+    String url="";
+
+    @Builder.Default
+    String errorView="";
+
+    @Builder.Default
+    String error = "";
     int errorStatus;
+
 }
