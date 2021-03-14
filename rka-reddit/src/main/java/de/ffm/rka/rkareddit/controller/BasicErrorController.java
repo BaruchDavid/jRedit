a497fa2623cf5c4d7bd722f78ec03d6bfbbf1ada
kaproma <kaproma@yahoo.de>
2021-03-14 14:23:55 +0100

    use errorObj after converting

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/BasicErrorController.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/BasicErrorController.java
@@ -1,9 +1,8 @@
 package de.ffm.rka.rkareddit.controller;
 
 
-import de.ffm.rka.rkareddit.domain.User;
+import de.ffm.rka.rkareddit.domain.dto.ErrorDTO;
 import de.ffm.rka.rkareddit.domain.dto.UserDTO;
-import de.ffm.rka.rkareddit.security.UserDetailsServiceImpl;
 import org.apache.commons.httpclient.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,59 +12,68 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 @Controller
-public class BasicErrorController implements ErrorController{
-	private static final Logger LOGGER = LoggerFactory.getLogger(BasicErrorController.class);
-	public static final String ANONYMOUS = "anonymousUser";
-	private final UserDetailsServiceImpl userDetailsService;
-	
-	@Override
-	public String getErrorPath() {
-		return "/error";
-	}
+public class BasicErrorController implements ErrorController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BasicErrorController.class);
+    private static final String USER_DTO = "userDto";
+    public static final String ANONYMOUS = "anonymousUser";
 
-	public BasicErrorController(UserDetailsServiceImpl userDetailsService) {
-		this.userDetailsService = userDetailsService;
-	}
-
-	@GetMapping("/error")
-    public String error(HttpServletRequest request, HttpServletResponse resp, Exception ex, Model model) {
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		UserDTO userDto = UserDTO.builder()
-						.firstName("Guest")
-						.secondName("")
-						.build();
-		if(!ANONYMOUS.equals(authentication.getName())){			
-			userDto =  getUserDTO(authentication.getName());
-		}
-		LOGGER.error("EXCEPTION {} REQUEST {} STATUS {}", ex, request.getRequestURL(), resp.getStatus());
-		model.addAttribute("userDto", userDto);
-		resp.setStatus(404);
-        return "error/pageNotFound";
+    @Override
+    public String getErrorPath() {
+        return "/error";
     }
-	
-	@GetMapping("/error/registrationError")
+
+
+    @GetMapping("/error")
+    public String error(@RequestParam(value = "errorDTO") ErrorDTO errorDTO, Model model, HttpServletResponse resp) {
+
+        String view;
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            view = errorDTO.getErrorView().isEmpty() ? "error/basicError" : errorDTO.getErrorView();
+            UserDTO userDto = !ANONYMOUS.equals(authentication.getName()) ?
+                    Optional.ofNullable(errorDTO.getLoggedUser())
+                            .orElseGet(() -> buildAnonymousUser()) : buildAnonymousUser();
+            LOGGER.error("EXCEPTION {} REQUEST {} STATUS {}", errorDTO.getError(), errorDTO.getErrorStatus(), errorDTO.getUrl());
+            model.addAttribute(USER_DTO, userDto);
+            resp.setStatus(errorDTO.getErrorStatus());
+        } catch (Exception e) {
+            LOGGER.error("EXCEPTION ON PROCESSING ERROR-HANDLER {} with orig exception {}", e.getMessage(), errorDTO.getError());
+            model.addAttribute(USER_DTO, buildAnonymousUser());
+            view = "error/pageNotFound";
+        }
+        return view;
+    }
+
+    private UserDTO buildAnonymousUser() {
+        Supplier<UserDTO> userDTOSupplier = () -> UserDTO.builder()
+                .firstName("Guest")
+                .secondName("")
+                .build();
+        return userDTOSupplier.get();
+    }
+
+    @GetMapping("/error/registrationError")
     public String registrationError(HttpServletRequest request, HttpServletResponse resp, Exception ex) {
-		LOGGER.error("SHOW REGISTRATION-ERROR-VEW {} WITH EXCEPTION {}", request.getRequestURI(), ex.getMessage());
-		resp.setStatus(HttpStatus.SC_BAD_REQUEST);
+        LOGGER.error("SHOW REGISTRATION-ERROR-VEW {} WITH EXCEPTION {}", request.getRequestURI(), ex.getMessage());
+        resp.setStatus(HttpStatus.SC_BAD_REQUEST);
         return "error/registrationError";
     }
 
-	@GetMapping("/error/accessDenied")
+    // TODO: 28.02.2021 getUserDTO(authentication.getName() ersetzen mit errorDto aus dem GlobalAdvisor
+    @GetMapping("/error/accessDenied")
     public String accessDenied(HttpServletRequest request, HttpServletResponse resp, Exception ex, Model model) {
-		LOGGER.error("SHOW ACCESS-DENIED-VEW {} WITH EXCEPTION {}", request.getRequestURI(), ex.getMessage());
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		model.addAttribute("userDto", getUserDTO(authentication.getName()));
-		resp.setStatus(403);
-		return "error/accessDenied";
+        LOGGER.error("SHOW ACCESS-DENIED-VEW {} WITH EXCEPTION {}", request.getRequestURI(), ex.getMessage());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute(USER_DTO, UserDTO.builder().build());
+        resp.setStatus(403);
+        return "error/accessDenied";
     }
-
-	private UserDTO getUserDTO(String userName) {
-		User user = (User) userDetailsService.loadUserByUsername(userName);
-		return  UserDTO.mapUserToUserDto(user);
-	}
 }
