de8d50af8b0edd9c69ccf59f39bdeb0a9baf95eb
kaproma <kaproma@yahoo.de>
2021-04-05 23:31:34 +0200

    without Authentication, you will redirected to login, no sence to check authentication

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/exception/GlobalAccessDeniedHandler.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/exception/GlobalAccessDeniedHandler.java
@@ -1,5 +1,6 @@
 package de.ffm.rka.rkareddit.exception;
 
+import de.ffm.rka.rkareddit.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.AccessDeniedException;
@@ -10,6 +11,7 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 public class GlobalAccessDeniedHandler implements AccessDeniedHandler{
 
@@ -20,15 +22,15 @@ public class GlobalAccessDeniedHandler implements AccessDeniedHandler{
 	public void handle(HttpServletRequest request, HttpServletResponse response,
 			AccessDeniedException accessDeniedException) throws IOException {
 		 Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-       if (auth != null) {
+		 String visitorName = Optional.ofNullable(((User)auth.getPrincipal()).getFirstName())
+				 					.orElse("Guest");
     	   LOGGER.warn("USER: {} WITH ADDRESS {} ATTEMPTED ILLEGAL ACCEsSS TO PROTECTED URL: {} ",auth.getName(), request.getRemoteAddr(), request.getRequestURI());
     	   if(request.getRequestURI().contains(LOGIN)
     		 || request.getRequestURI().contains(REGISTER)) {
     		   response.sendRedirect(request.getContextPath().concat("/links"));
     	   } else {
-    		   response.sendRedirect(request.getContextPath().concat("/error/accessDenied"));
+    		   response.sendRedirect(request.getContextPath().concat("/error/accessDenied?name="+visitorName));
     	   }
-       }
 	}
 
 }
