3ce225efbdfac212c21bce1d1b77b37de60e160b
kaproma <kaproma@yahoo.de>
2021-07-25 00:17:21 +0200

    change user to real email for recovering purposes

--- rka-reddit/src/main/resources/sql/dml/data-h2.sql
+++ rka-reddit/src/main/resources/sql/dml/data-h2.sql
@@ -1,5 +1,5 @@
 INSERT INTO User(user_id, email, enabled, password, first_name, second_name, alias_name, new_email, profile_foto, foto_creation_Date, confirm_password,creation_date)
-VALUES(1, 'romakapt@gmx.de', 1, '$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne', 'baruc-david', 'rka', 'worker', 'kaproma@yahoo.de', null, null, '$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne', NOW());
+VALUES(1, 'kaproma@yahoo.de', 1, '$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne', 'baruc-david', 'rka', 'worker', 'kaproma@yahoo.de', null, null, '$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne', NOW());
 
 INSERT INTO User(user_id, email, enabled, password, first_name, second_name, alias_name, new_email, profile_foto, foto_creation_Date, confirm_password, creation_date)
 VALUES(2, 'dascha@gmx.de', 1, '$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne', 'dascha', 'unknown', 'ceo', '', null, null, '$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne', NOW());
