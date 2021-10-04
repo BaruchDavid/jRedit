4f522b9912f173cec32d30b01cb813fa65e55a2a
kaproma <kaproma@yahoo.de>
2021-10-04 20:43:19 +0200

    add new controller for pw-recovering

---
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/PwRecoverController.java
@@ -0,0 +1,107 @@
+package de.ffm.rka.rkareddit.controller;
+
+import de.ffm.rka.rkareddit.domain.dto.UserDTO;
+import de.ffm.rka.rkareddit.domain.validator.user.UserValidationgroup;
+import de.ffm.rka.rkareddit.exception.ServiceException;
+import de.ffm.rka.rkareddit.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import de.ffm.rka.rkareddit.domain.validator.user.UserValidationgroup.UnauthenticatedUserRecoverPassword;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@Controller
+public class PwRecoverController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PwRecoverController.class);
+    private static final String LOGGED_IN_USER = "userDto";
+    private static final String CONTENT_USER = "userContent";
+    private static final String SUCCESS = "success";
+    private static final String REDIRECT_MESSAGE = "redirectMessage";
+    private static final String BINDING_ERROR = "bindingError";
+    private static final String VALIDATION_ERRORS = "validationErrors";
+    private static final String ERROR_MESSAGE = "Update user validation Error: {} message: {}";
+    private static final String REDIRECT_TO_PRIVATE_PROFILE = "redirect:/profile/private";
+    private final UserService userService;
+
+
+    public PwRecoverController(UserService userService) {
+        this.userService = userService;
+
+    }
+
+    @GetMapping("/profile/user/recover/view")
+    public String showRecoverUserPwRequest(Model model) {
+        LOGGER.info("SHOW VIEW FOR PASSWORD RECOVERING {}");
+        model.addAttribute(LOGGED_IN_USER, UserDTO.builder().email("notLoggedIn").build());
+        model.addAttribute(CONTENT_USER, UserDTO.builder().firstName("Guest").build());
+        return "recover/recoverUserPwRequest";
+    }
+
+    @GetMapping("/profile/user/recover/{email}/{activationCode}")
+    public String getPasswordRecoveryForm(@PathVariable String email, @PathVariable String activationCode, Model model)
+            throws ServiceException {
+        LOGGER.info("TRY TO SHOW PW-RECOVER-VIEW FOR USER {} ON GIVEN ACTIVATION-CODE ", email, activationCode);
+        String returnLink = "recover/passwordRecoveryForm";
+        Optional<UserDTO> userDTO = userService.getUserForPasswordReset(email, activationCode);
+        if (userDTO.isPresent()) {
+            model.addAttribute(LOGGED_IN_USER, UserDTO.builder().email("notLoggedIn").build());
+            model.addAttribute(CONTENT_USER, UserDTO.builder().email(email).build());
+            LOGGER.error("USER {} PASSED ACTIVATION-CODE {} FOR PW-RECOVERING SUCCESSFULLY", email, activationCode);
+            return returnLink;
+        } else {
+            LOGGER.error("USER {} PASSED ACTIVATION-CODE {} FOR PW-RECOVERING NOT SUCCESSFULLY", email, activationCode);
+            return "redirect:/error/registrationError";
+        }
+    }
+
+
+    @PostMapping("/profile/user/recover/")
+    public String createActivationCodeAndSendMail(@RequestParam String userEmail, RedirectAttributes attributes, Model model) {
+        LOGGER.info("TRY TO CREATE ACTIVATION CODE AND SEND MAIL WITH EMAIL FOR PW-RECOVERING {}", userEmail);
+        userService.findUserById(userEmail)
+                .ifPresent(user -> userService.saveNewActionCode(user));
+        model.addAttribute(LOGGED_IN_USER, UserDTO.builder().email("notLoggedIn").build());
+        model.addAttribute(CONTENT_USER, UserDTO.builder().firstName("Guest").build());
+        attributes.addFlashAttribute(SUCCESS, true);
+        LOGGER.info("ACTIVATION CODE AND SEND MAIL WITH EMAIL FOR PW-RECOVERING SUCCESSFUL {}", userEmail);
+        return "recover/recoverUserPwRequestApplied";
+
+    }
+
+    @PutMapping("/profile/user/recover")
+    public String userPasswordRecovery(@Validated(UnauthenticatedUserRecoverPassword.class) UserDTO userDto,
+                                       BindingResult bindingResult, HttpServletResponse res, RedirectAttributes attributes, Model model) {
+        LOGGER.info("TRY TO SAVE NEW PASSWORD FOR PW-RECOVERING FOR USER{}", userDto.getEmail());
+        if (bindingResult.hasErrors()) {
+            manageValidationErrors(userDto, bindingResult, res, attributes, model);
+            //setSameUserForLoginAndContent((User) userDetails, model);
+            return "auth/passwordChange";
+        } else {
+            userService.changeUserPassword(userDto);
+            attributes.addFlashAttribute(SUCCESS, true);
+            attributes.addFlashAttribute(REDIRECT_MESSAGE, "your password has been changed!");
+            res.setStatus(HttpStatus.PERMANENT_REDIRECT.value());
+            LOGGER.info("USER PASSWORD RECOVERED SUCCESSFULLY {}", userDto.getEmail());
+            return REDIRECT_TO_PRIVATE_PROFILE;
+        }
+    }
+
+    private void manageValidationErrors(@Validated(UserValidationgroup.ValidationChangeUserProperties.class) UserDTO userDto,
+                                        BindingResult bindingResult, HttpServletResponse res,
+                                        RedirectAttributes attributes, Model model) {
+        bindingResult.getAllErrors().forEach(error -> LOGGER.warn(ERROR_MESSAGE,
+                error.getCodes(), error.getDefaultMessage()));
+        model.addAttribute(VALIDATION_ERRORS, bindingResult.getAllErrors());
+        model.addAttribute(LOGGED_IN_USER, userDto);
+        attributes.addFlashAttribute(BINDING_ERROR, true);
+        res.setStatus(HttpStatus.BAD_REQUEST.value());
+    }
+}
