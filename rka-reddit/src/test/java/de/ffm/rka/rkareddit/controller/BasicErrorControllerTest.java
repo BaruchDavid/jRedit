73f02623dcd13108708276c32506874daa8693d1
kaproma <kaproma@yahoo.de>
2022-02-15 18:22:47 +0100

    move to MvcRequestSender

--- rka-reddit/src/test/java/de/ffm/rka/rkareddit/controller/BasicErrorControllerTest.java
+++ rka-reddit/src/test/java/de/ffm/rka/rkareddit/controller/BasicErrorControllerTest.java
@@ -1,53 +1,25 @@
 package de.ffm.rka.rkareddit.controller;
 
 
-import de.ffm.rka.rkareddit.config.SpringSecurityTestConfig;
 import de.ffm.rka.rkareddit.util.BeanUtil;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import javax.persistence.EntityManager;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@ActiveProfiles("test")
-/** spring-test-support is enabled */
-@RunWith(SpringRunner.class) 
-/** enable of application-context */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringSecurityTestConfig.class)
-@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
-public class BasicErrorControllerTest {
+public class BasicErrorControllerTest extends MvcRequestSender{
 
-	private MockMvc mockMvc;
 
-	@Autowired
-	private WebApplicationContext context;
-	
 	private EntityManager entityManager;
 	
 	@Before
 	public void setup() {
 
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-										.apply(springSecurity())
-										.build();
 		entityManager = BeanUtil.getBeanFromContext(EntityManager.class);
 	}
 
@@ -58,8 +30,8 @@ public class BasicErrorControllerTest {
 	@Test
 	@WithUserDetails("grom@gmx.de")
 	public void accessDeniePage() throws Exception {
-		final MvcResult mvcResult = this.mockMvc.perform(get("/data/h2-console/**"))
-				.andDo(print())
+
+		final MvcResult mvcResult = super.performGetRequest("/data/h2-console/**")
 				.andExpect(status().is(302))
 				.andReturn();
 		mvcResult.getResponse().getRedirectedUrl().contains("/error/accessDenied");
@@ -68,8 +40,7 @@ public class BasicErrorControllerTest {
 	@Test
 	@WithUserDetails("dascha@gmx.de")
 	public void requestDirektAccessDenied() throws Exception {
-		this.mockMvc.perform(get("/error/accessDenied"))
-					.andDo(print())
+		super.performGetRequest("/error/accessDenied")
 					.andExpect(status().is(403))
 					.andReturn();
 	}
@@ -77,16 +48,14 @@ public class BasicErrorControllerTest {
 	@Test
 	@WithUserDetails("dascha@gmx.de")
 	public void requestDirektErrorPage() throws Exception {
-		this.mockMvc.perform(get("/error"))
-					.andDo(print())
+		super.performGetRequest("/error")
 					.andExpect(status().is(404))
 					.andReturn();
 	}
 	
 	@Test
 	public void requestDirektErrorPageAsAnonoymus() throws Exception {
-		this.mockMvc.perform(get("/error"))
-					.andDo(print())
+		super.performGetRequest("/error")
 					.andExpect(status().is(404))
 					.andReturn();
 	}
