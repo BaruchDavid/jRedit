73bc61b1ebd57f6aa52f8a89474f53f6ffcb285e
kaproma <kaproma@yahoo.de>
2022-02-15 17:49:58 +0100

    move TagControllerTest on MvcRequestSender

--- rka-reddit/src/test/java/de/ffm/rka/rkareddit/controller/TagControllerTest.java
+++ rka-reddit/src/test/java/de/ffm/rka/rkareddit/controller/TagControllerTest.java
@@ -1,49 +1,24 @@
 package de.ffm.rka.rkareddit.controller;
 
 
-import de.ffm.rka.rkareddit.config.SpringSecurityTestConfig;
 import de.ffm.rka.rkareddit.exception.GlobalControllerAdvisor;
-import de.ffm.rka.rkareddit.interceptor.ApplicationHandlerInterceptor;
 import de.ffm.rka.rkareddit.rest.controller.TagController;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.security.web.method.annotation.AuthenticationPrincipalArgumentResolver;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ActiveProfiles("test")
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringSecurityTestConfig.class)
-@DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
-public class TagControllerTest {
+public class TagControllerTest extends MvcRequestSender{
 
-	private MockMvc mockMvc;
 
-	@Autowired
-	private TagController tagController;
-
-	@Autowired
-	private GlobalControllerAdvisor globalControllerAdvice;
-
-	
 	/**
 	 * Using Standalone-Configuration, no SpringApplicationContext.
 	 * All additional elements (filter, advices, interceptors) must be set manualy
@@ -51,12 +26,12 @@ public class TagControllerTest {
 	@Before
 	public void setup() {
 
-        MockitoAnnotations.initMocks(this);
+       /* MockitoAnnotations.initMocks(this);
 		this.mockMvc = MockMvcBuilders.standaloneSetup(tagController)
 										.addInterceptors(new ApplicationHandlerInterceptor())
 										.setControllerAdvice(globalControllerAdvice)		
 										.setCustomArgumentResolvers(new AuthenticationPrincipalArgumentResolver(), new PageableHandlerMethodArgumentResolver())
-										.build();
+										.build();*/
 	}
 	
 	/**
@@ -68,10 +43,9 @@ public class TagControllerTest {
 	@WithUserDetails("kaproma@yahoo.de")
 	public void postNewComment() throws Exception {
 
-		MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.post("/tags/tag/create")
-																	.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-																	.param("java8", ""))
-	    					.andDo(print())
+		String body = "java8=";
+
+		MvcResult result = super.performPostRequest("/tags/tag/create", body)
 							.andReturn();
 		assertNotEquals("1", result.getResponse().getContentAsString().split(":")[2]);
 	}
@@ -79,11 +53,8 @@ public class TagControllerTest {
 	@Test
 	@WithUserDetails("kaproma@yahoo.de")
 	public void postAvailibleTag() throws Exception {
-
-		MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.post("/tags/tag/create")
-														.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-														.param("java", ""))
-	    					.andDo(print())
+		String body = "java=";
+		MvcResult result = super.performPostRequest("/tags/tag/create", body)
 							.andReturn();
 		assertNotEquals("1", result.getResponse().getContentAsString().split(":")[2]);
 	}
@@ -92,7 +63,7 @@ public class TagControllerTest {
 	@WithUserDetails("kaproma@yahoo.de")
 	public void testSchouldNotDeleteRelatedTag() throws Exception {
 
-		MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.delete("/tags/tag/deleteTag/{tagId}","1")
+		MvcResult result = super.mockMvc.perform(MockMvcRequestBuilders.delete("/tags/tag/deleteTag/{tagId}","1")
 																	.contentType(MediaType.APPLICATION_JSON))
 							.andDo(print())
 							.andExpect(status().isOk())
