85157703519017b66ecffc5a75220113b23ef0a2
kaproma <kaproma@yahoo.de>
2022-05-29 22:06:13 +0200

    use tagNum on from tagDto

--- rka-reddit/src/main/resources/static/js/tag.js
+++ rka-reddit/src/main/resources/static/js/tag.js
@@ -1,5 +1,5 @@
 $('.tag-input').focusout(function(event){
 		var tag = event.target;
-		document.getElementById(tag.id+".tagId").value = null;
+		document.getElementById(tag.id+".tagNum").value = null;
 		document.getElementById(tag.id+".tagName").value = tag.value;
 });
