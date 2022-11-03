623311dd5797e680ab5ef9e13f8857bfa097678f
Kap Roma <roman.kaptsan-extern@deutschebahn.com>
2022-11-03 22:40:18 +0100

    dont use application arguments, use environment vars instead

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/RkaRedditApplication.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/RkaRedditApplication.java
@@ -1,33 +1,19 @@
 package de.ffm.rka.rkareddit;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
 public class RkaRedditApplication {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(RkaRedditApplication.class);
-	private static final String PROD = "prod";
-	private static final String DEV = "dev";
-	private static final String TEST = "test";
+
 	
 	public static void main(String[] args) {
-		LOGGER.info("START JREDITT APPLICATION with ARGS {}", List.of(args));
-		String environment = args[0].substring(args[0].indexOf('=')+1);
-		if(!PROD.equals(environment) 
-			&& !DEV.equals(environment)
-			&& !TEST.equalsIgnoreCase(environment)) {
-			throw new IllegalArgumentException("INVALID RUNTIME-ENVIRONMENT: ".concat(environment));
-		}
 		SpringApplication.run(RkaRedditApplication.class, args);
 	}
 	
