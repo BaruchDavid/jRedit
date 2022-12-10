8d69ffe8b75329ae53b8cad65fda612b67e98267
Baruch David <roman.kaptsan-extern@deutschebahn.com>
2022-12-10 23:08:35 +0100

    BUG FIX: select distinct tagName

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/repository/TagRepository.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/repository/TagRepository.java
@@ -10,15 +10,15 @@ import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-	@Query(value = "SELECT tagName FROM Tag WHERE UPPER(tagName) LIKE UPPER(CONCAT('%', :tagName,'%'))")
-	List<String> findTagByName(@Param("tagName") String tagName);
-	
-	@Query(value = "SELECT tag FROM Tag tag WHERE UPPER(tagName) LIKE UPPER(:tagName)")
-	Optional<Tag> findByName(String tagName);
-	
-	@Query(value = "SELECT tg.tagId FROM Tag tg JOIN tg.links lk  WHERE tg.tagId =:tagId")
-	Optional<String> selectTagIdFromRelation(String tagId);
+    @Query(value = "SELECT DISTINCT tagName FROM Tag WHERE UPPER(tagName) LIKE UPPER(CONCAT('%', :tagName,'%'))")
+    List<String> findTagByName(@Param("tagName") String tagName);
 
-	@Query(value = "SELECT a FROM Tag a LEFT OUTER JOIN FETCH a.links WHERE a.tagId =:tagId")
-	Tag selectTagWithLinks(@Param("tagId") long tagId);
+    @Query(value = "SELECT tag FROM Tag tag WHERE UPPER(tagName) LIKE UPPER(:tagName)")
+    Optional<Tag> findByName(String tagName);
+
+    @Query(value = "SELECT tg.tagId FROM Tag tg JOIN tg.links lk  WHERE tg.tagId =:tagId")
+    Optional<String> selectTagIdFromRelation(String tagId);
+
+    @Query(value = "SELECT a FROM Tag a LEFT OUTER JOIN FETCH a.links WHERE a.tagId =:tagId")
+    Tag selectTagWithLinks(@Param("tagId") long tagId);
 }
