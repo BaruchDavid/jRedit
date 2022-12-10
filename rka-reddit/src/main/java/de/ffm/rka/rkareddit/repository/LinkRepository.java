bf591f831e5f9ff1c426d61723f5c56f5b6233da
Baruch David <roman.kaptsan-extern@deutschebahn.com>
2022-12-10 21:19:15 +0100

    add spring-data named-query by column

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/repository/LinkRepository.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/repository/LinkRepository.java
@@ -12,57 +12,59 @@ import java.util.Optional;
 import java.util.Set;
 
 public interface LinkRepository extends JpaRepository<Link, Long> {
-	
-	/**
-	 * namedQuery, SpringData introduces count-statement
-	 */
-	long countByUser(User usr);
 
-	/**
-	 * find all comments for each user
-	 */
-	@Query(value = "SELECT l " +
-		   "FROM Link l " +
-		   "INNER JOIN FETCH l.user ORDER BY l.linkId DESC",
-		   countQuery = "SELECT COUNT(l) FROM Link l INNER JOIN l.user")
-	Page<Link> findAll(Pageable pageable);
+    /**
+     * namedQuery, SpringData introduces count-statement
+     */
+    long countByUser(User usr);
+
+    /**
+     * find all comments for each user
+     */
+    @Query(value = "SELECT l " +
+            "FROM Link l " +
+            "INNER JOIN FETCH l.user ORDER BY l.linkId DESC",
+            countQuery = "SELECT COUNT(l) FROM Link l INNER JOIN l.user")
+    Page<Link> findAll(Pageable pageable);
 
     Optional<Link> findByLinkId(long id);
 
     @Query("SELECT l "
-    		+ "FROM Link l "
-    		+ "INNER JOIN FETCH l.tags "
-    		+ "WHERE l.linkId =:id ")
-	Link findTagsForLink(@Param("id") long id);
+            + "FROM Link l "
+            + "INNER JOIN FETCH l.tags "
+            + "WHERE l.linkId =:id ")
+    Link findTagsForLink(@Param("id") long id);
 
-	@Query("SELECT l "
-			+ "FROM Link l "
-			+ "INNER JOIN FETCH l.user "
-			+ "WHERE l.linkId =:id ")
-	Optional<Link> findLinkWithUserByLinkId(long id);
+    @Query("SELECT l "
+            + "FROM Link l "
+            + "INNER JOIN FETCH l.user "
+            + "WHERE l.linkId =:id ")
+    Optional<Link> findLinkWithUserByLinkId(long id);
 
-	@Query("SELECT l "
-			+ "FROM Link l "
-			+ "LEFT OUTER JOIN FETCH l.comments "
-			+ "WHERE l.linkId IN (:linkIds) ")
-	Set<Link> findLinksWithComments(@Param("linkIds") Set<Long> linkIds);
+    @Query("SELECT l "
+            + "FROM Link l "
+            + "LEFT OUTER JOIN FETCH l.comments "
+            + "WHERE l.linkId IN (:linkIds) ")
+    Set<Link> findLinksWithComments(@Param("linkIds") Set<Long> linkIds);
 
-	@Query("SELECT l "
-			+ "FROM Link l "
-			+ "LEFT OUTER JOIN FETCH l.comments com "
-			+ "JOIN FETCH com.user usr "
-			+ "WHERE usr.email =:email ")
+    @Query("SELECT l "
+            + "FROM Link l "
+            + "LEFT OUTER JOIN FETCH l.comments com "
+            + "JOIN FETCH com.user usr "
+            + "WHERE usr.email =:email ")
     Set<Link> findLinkWithUserComments(@Param("email") String username);
 
 
-	@Query(value = "SELECT l " +
-			"FROM Link l " +
-			"INNER JOIN FETCH l.user " +
-			"LEFT OUTER JOIN l.tags relTable " +
-			"WHERE relTable.tagId IN (SELECT tagId " +
-										"FROM Tag " +
-										"WHERE tagName =:tag)",
-			countQuery = "SELECT COUNT(l) FROM Link l INNER JOIN l.user")
-	Page<Link> findLinksOnTag(@Param("tag") String tag, Pageable pageable);
+    @Query(value = "SELECT l " +
+            "FROM Link l " +
+            "INNER JOIN FETCH l.user " +
+            "LEFT OUTER JOIN l.tags relTable " +
+            "WHERE relTable.tagId IN (SELECT tagId " +
+            "FROM Tag " +
+            "WHERE tagName =:tag)",
+            countQuery = "SELECT COUNT(l) FROM Link l INNER JOIN l.user")
+    Page<Link> findLinksOnTag(@Param("tag") String tag, Pageable pageable);
+
+    Optional<Link> findByUrl(String url);
 
 }
