473a673024172d4f1603d0b69eb65f6a8ceb2083
Baruch David <roman.kaptsan-extern@deutschebahn.com>
2022-12-10 21:23:27 +0100

    add spring-data named-query by column

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/domain/validator/link/UniqueUrlValidator.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/domain/validator/link/UniqueUrlValidator.java
@@ -1,11 +1,28 @@
 package de.ffm.rka.rkareddit.domain.validator.link;
 
+import de.ffm.rka.rkareddit.repository.LinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class UniqueUrlValidator implements ConstraintValidator<UniqueUrl, String> {
+
+
+    @Autowired
+    LinkRepository linkRepository;
+
+    /**
+     * if Link is present, it is true. On Validation view it is false,
+     * because, when link is present, it is a not valid link-url,
+     * that's why negotiation will be used
+     *
+     * @param linkUrl                    which should be saved
+     * @param constraintValidatorContext containts context
+     * @return true, when url is not present
+     */
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public boolean isValid(String linkUrl, ConstraintValidatorContext constraintValidatorContext) {
+        return linkRepository.findByUrl(linkUrl).isEmpty();
     }
 }
