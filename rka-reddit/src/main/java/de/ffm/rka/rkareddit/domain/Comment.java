a8387799c3b5afb181928e42c614f9bdde4cad13
Baruch David <roman.kaptsan-extern@deutschebahn.com>
2023-01-13 17:00:24 +0100

    Comment save with SEQUENCE-Strategy

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/domain/Comment.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/domain/Comment.java
@@ -17,58 +17,60 @@ import static java.util.Date.from;
 
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @ToString(exclude = {"link", "user"})
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
-public class Comment extends Auditable implements Serializable{
-	
-	private static final long serialVersionUID = -5839947949942907414L;
+public class Comment extends Auditable implements Serializable {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long commentId;
+    private static final long serialVersionUID = -5839947949942907414L;
 
-	@Column(length = 600)
-	private String commentText;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "comment_generator")
+    @SequenceGenerator(name = "comment_generator", sequenceName = "COMMENT_SEQ")
+    private Long commentId;
 
-	@JsonIgnore
-	@NotNull
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "link_id")
-	private Link link;
+    @Column(length = 600)
+    private String commentText;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JsonIgnore
-	@JoinColumn(name = "user_id")
-	private User user;
-	
-	@Autowired
-	private transient PrettyTime prettyTime;
+    @JsonIgnore
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "link_id")
+    private Link link;
 
-	
-	private static final ZoneId ZONE_ID = ZoneId.systemDefault();
-	
-	public String getElapsedTime() {
-		prettyTime = BeanUtil.getBeanFromContext(PrettyTime.class);
-		return prettyTime.format(from(super.getCreationDate().atZone(ZONE_ID).toInstant()));
-	}
-	
-	@Override
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Autowired
+    private transient PrettyTime prettyTime;
+
+
+    private static final ZoneId ZONE_ID = ZoneId.systemDefault();
+
+    public String getElapsedTime() {
+        prettyTime = BeanUtil.getBeanFromContext(PrettyTime.class);
+        return prettyTime.format(from(super.getCreationDate().atZone(ZONE_ID).toInstant()));
+    }
+
+    @Override
     public boolean equals(Object o) {
-		boolean result;
-		if (this == o) {
-			result = true;
-		} else if (!(o instanceof Comment)) {
-			result = false;
-		} else {
-			Comment other = (Comment) o;
-			result = commentText.equals(other.getCommentText());
-		}
-		return result;
-	}
-	 
+        boolean result;
+        if (this == o) {
+            result = true;
+        } else if (!(o instanceof Comment)) {
+            result = false;
+        } else {
+            Comment other = (Comment) o;
+            result = commentText.equals(other.getCommentText());
+        }
+        return result;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(commentText);
--- rka-reddit/src/main/resources/sql/dml/data-h2.sql
+++ rka-reddit/src/main/resources/sql/dml/data-h2.sql
@@ -147,52 +147,55 @@ INSERT INTO users_roles(user_Id, role_Id)
 VALUES (3, 1);
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (1, 'Erstes Kommentar von romakapt', 1, 1, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'Erstes Kommentar von romakapt', 1, 1, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (2, 'zweites kommentar von dascha ', 21, 2, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'zweites kommentar von dascha ', 21, 2, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (3, 'drittes kommentar von grom', 121, 3, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'drittes kommentar von grom', 121, 3, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (10, '10 Kommentar von romakapt', 121, 1, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), '10 Kommentar von romakapt', 121, 1, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (4, 'Viertes Kommentar für zweiten Link von dascha', 101, 2, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'Viertes Kommentar für zweiten Link von dascha',
+        101, 2, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (5, 'fünftes Kommentar für zweiten Link von dascha', 201, 2, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'fünftes Kommentar für zweiten Link von dascha',
+        201, 2, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (6, 'sechstes kommentar von dascha.', 201, 2, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'sechstes kommentar von dascha.', 201, 2,
+        NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (7, 'siebtes kommentar von dascha.', 81, 2, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'siebtes kommentar von dascha.', 81, 2, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (8, 'achtes kommentar von dascha.', 21, 2, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'achtes kommentar von dascha.', 21, 2, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (9, 'neuntes kommentar von grom.', 41, 3, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'neuntes kommentar von grom.', 41, 3, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (12, 'zwölftes kommentar von grom.', 41, 3, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), 'zwölftes kommentar von grom.', 41, 3, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (22, '22 kommentar von grom.', 41, 3, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), '22 kommentar von grom.', 41, 3, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (23, '23 kommentar von grom', 41, 3, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), '23 kommentar von grom', 41, 3, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (24, '24 kommentar von grom', 61, 3, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), '24 kommentar von grom', 61, 3, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (25, '25 kommentar von grom', 61, 3, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), '25 kommentar von grom', 61, 3, NOW());
 
 INSERT INTO Comment(comment_id, comment_text, link_id, user_id, creation_Date)
-VALUES (26, '26 kommentar von grom', 61, 3, NOW());
+VALUES (NEXTVAL('COMMENT_SEQ'), '26 kommentar von grom', 61, 3, NOW());
 
 INSERT INTO Tag(tag_id, tag_name)
 VALUES (NEXTVAL('TAG_SEQ'), 'Java');
