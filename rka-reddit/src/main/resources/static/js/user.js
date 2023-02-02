cb2f3febb74701c04a987e382661c61bcb64edc0
Baruch David <roman.kaptsan-extern@deutschebahn.com>
2023-02-02 21:16:19 +0100

    show linksignature in frontend

--- rka-reddit/src/main/resources/static/js/user.js
+++ rka-reddit/src/main/resources/static/js/user.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
-	const user = $('#userName').attr("name");
-	if(user !== undefined){
-        fetch ('/jReditt/profile/information/userClickedLinks?user='+user, {method: 'GET'})
+$(document).ready(function () {
+    const user = $('#userName').attr("name");
+    if (user !== undefined) {
+        fetch('/jReditt/profile/information/userClickedLinks?user=' + user, {method: 'GET'})
             .then(function (response) {
-               return response.json();
-            }).then (function (data) {
-                  let ul = $('#linkHis');
-                  if (data.length === 0) {
-                    $('#linkHisTitle').css('display', 'none');
-                  } else {
-                      $.each( data, function( key, value ) {
-                          let li = $('<li class="list-group-item">').appendTo(ul);
-                          $('<a>',{
-                              text: value.title,
-                              title: value.title,
-                              href: '/jReditt/links/link/' + value.linkId
-                          }).appendTo(li);
-                      });
-                  }
-            }).catch (function (error) {
-               console.log ("error: " + error);
-            });
-	}
+                return response.json();
+            }).then(function (data) {
+            let ul = $('#linkHis');
+            if (data.length === 0) {
+                $('#linkHisTitle').css('display', 'none');
+            } else {
+                $.each(data, function (key, value) {
+                    let li = $('<li class="list-group-item">').appendTo(ul);
+                    $('<a>', {
+                        text: value.title,
+                        title: value.title,
+                        href: '/jReditt/links/link/' + value.linkSignature
+                    }).appendTo(li);
+                });
+            }
+        }).catch(function (error) {
+            console.log("error: " + error);
+        });
+    }
 });
 
-$( "#displayMessage" ).show(function() {
-setTimeout(() => {
-	$('#displayMessage').css('display', 'none');
-}, 3000);
+$("#displayMessage").show(function () {
+    setTimeout(() => {
+        $('#displayMessage').css('display', 'none');
+    }, 3000);
 });
 	
