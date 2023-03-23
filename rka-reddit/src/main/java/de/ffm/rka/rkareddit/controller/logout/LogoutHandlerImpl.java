ab1bf5d663e8184535b8e7f44c66bf7691aadb07
Baruch David <roman.kaptsan-extern@deutschebahn.com>
2023-03-23 12:12:59 +0100

    remove user from cache on logout, add LogoutHandlerImpl to Sec-Config

---
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/controller/logout/LogoutHandlerImpl.java
@@ -0,0 +1,52 @@
+package de.ffm.rka.rkareddit.controller.logout;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@Component
+public class LogoutHandlerImpl implements LogoutHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogoutHandlerImpl.class);
+
+    final
+    CacheManager cacheManager;
+
+    public LogoutHandlerImpl(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+        cleanUserCache(authentication.getName());
+
+
+        LOGGER.info("USER {} HAS BEEN LOGOUT", authentication.getName());
+    }
+
+    //@CacheEvict(value = "users", key = "#email")
+    public void cleanUserCache(String mail) {
+        final Cache users = Optional.ofNullable(cacheManager.getCache("users"))
+                .orElseGet(() -> new ConcurrentMapCache("noUserCache"));
+
+        final boolean cacheCleaned = Optional.of(users)
+                .map(cache -> cache.evictIfPresent(mail))
+                .orElse(false);
+        LOGGER.info("CACHE FOR USER {} HAS BEEN CLEANED {}", mail, cacheCleaned);
+        LOGGER.info("USER-CACHE CONTAINS {} USERS", ((ConcurrentMapCache) Optional.of(users)
+                .orElseGet(() -> new ConcurrentMapCache("noUsers")))
+                .getNativeCache().size());
+
+
+    }
+}
--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/security/SecConfig.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/security/SecConfig.java
@@ -1,9 +1,11 @@
 package de.ffm.rka.rkareddit.security;
 
+import de.ffm.rka.rkareddit.controller.logout.LogoutHandlerImpl;
 import de.ffm.rka.rkareddit.exception.GlobalAccessDeniedHandler;
 import de.ffm.rka.rkareddit.service.UserDetailsServiceImpl;
 import de.ffm.rka.rkareddit.util.BeanUtil;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -25,98 +27,102 @@ import static de.ffm.rka.rkareddit.security.Role.*;
 /**
  * This is a configuration service for authentication
  * secures role-based requests and url-based requests
- * @author rka
  *
+ * @author rka
  */
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecConfig extends WebSecurityConfigurerAdapter {
 
-	@Bean
-    public AuthenticationSuccessHandler userSuccessfulAuthenticationHandler(){
+    @Autowired
+    CacheManager cacheManager;
+
+    @Bean
+    public AuthenticationSuccessHandler userSuccessfulAuthenticationHandler() {
         return new UserSuccessfullAthenticationHandler();
     }
 
-	@Bean
-    public AuthenticationFailureHandler userFailureAuthenticationHandler(){
+    @Bean
+    public AuthenticationFailureHandler userFailureAuthenticationHandler() {
         return new UserFailureAuthenticationHandler();
     }
 
-	@Bean
-    public UserDetailsService getUserDetailsService(){
+    @Bean
+    public UserDetailsService getUserDetailsService() {
         return new UserDetailsServiceImpl();
     }
 
-	@Bean
-	public AccessDeniedHandler getAccessDeniedHandler() {
-		return new GlobalAccessDeniedHandler();
-	}
+    @Bean
+    public AccessDeniedHandler getAccessDeniedHandler() {
+        return new GlobalAccessDeniedHandler();
+    }
 
 
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		final int oneDay = 86400;
-		http.csrf().disable()
- 					.headers().frameOptions().disable()
- 			.and()
- 			.authorizeRequests()
-							.antMatchers("/","/links/","/resources/**").permitAll()
-							.antMatchers("/profile/public").permitAll()
-							.antMatchers(HttpMethod.GET, "/links/link/{signature}").permitAll()
-							.antMatchers("/login*").hasRole(ANONYMOUS.name())
-							.antMatchers("/registration").hasRole(ANONYMOUS.name())
-							.antMatchers(HttpMethod.GET, "/profile/private").hasRole(USER.name())
-							.antMatchers(HttpMethod.GET, "/profile/private/me").hasRole(USER.name())
-							.antMatchers(HttpMethod.GET, "/profile/private/me/password").hasRole(USER.name())
-							.antMatchers(HttpMethod.GET, "/profile/private/me/update/email").hasRole(USER.name())
-							.antMatchers(HttpMethod.GET, "/links/link").hasRole(USER.name())
-							.antMatchers(HttpMethod.PATCH, "/profile/private/me/update/email").hasRole(USER.name())
-							.antMatchers(HttpMethod.PUT, "/profile/private/me/update").hasRole(USER.name())
-							.antMatchers(HttpMethod.PUT, "/profile/private/me/password").hasRole(USER.name())
-							.antMatchers("/link/{lSig}/vote/direction/{direction}/votecount/{voteCount}").hasRole(USER.name())
-							.antMatchers(HttpMethod.DELETE, "/tag/deleteTag/{tagId}").hasRole(ADMIN.name())
-							.antMatchers(HttpMethod.POST, "/tags/tag/create").hasRole(ADMIN.name())
-							.antMatchers(HttpMethod.POST, "/links/link").hasRole(USER.name())
-							.antMatchers(HttpMethod.POST, "/comments/comment").hasRole(USER.name())
-							.antMatchers("/data/h2-console/**").hasRole(DBA.name())
-							//.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole(ACTUATOR.name())
-			.and()
-				.requiresChannel(requestRegistry -> requestRegistry.anyRequest().requiresSecure())
-				.authorizeRequests(interceptUrlRegistry -> interceptUrlRegistry.anyRequest().permitAll())
-			.formLogin().loginPage("/login")
-						.usernameParameter("email")
-						.successHandler(userSuccessfulAuthenticationHandler())
-						.failureHandler(userFailureAuthenticationHandler())
-						.failureUrl("/login?error")
-			.and()
-			.exceptionHandling().accessDeniedHandler(getAccessDeniedHandler())
-			.and()
-			.logout().deleteCookies("JSESSIONID")
-					.logoutUrl("/logout")
-					.invalidateHttpSession(true)
-					.clearAuthentication(true)
-			.and()
-			.rememberMe().key("uniqueAndSecret")
-						 .tokenValiditySeconds(oneDay)
-			.and()
-			.sessionManagement()
-						.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-						.maximumSessions(1)
-						.expiredUrl("/login?oneSession");
-	}
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        final int oneDay = 86400;
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/links/", "/resources/**").permitAll()
+                .antMatchers("/profile/public").permitAll()
+                .antMatchers(HttpMethod.GET, "/links/link/{signature}").permitAll()
+                .antMatchers("/login*").hasRole(ANONYMOUS.name())
+                .antMatchers("/registration").hasRole(ANONYMOUS.name())
+                .antMatchers(HttpMethod.GET, "/profile/private").hasRole(USER.name())
+                .antMatchers(HttpMethod.GET, "/profile/private/me").hasRole(USER.name())
+                .antMatchers(HttpMethod.GET, "/profile/private/me/password").hasRole(USER.name())
+                .antMatchers(HttpMethod.GET, "/profile/private/me/update/email").hasRole(USER.name())
+                .antMatchers(HttpMethod.GET, "/links/link").hasRole(USER.name())
+                .antMatchers(HttpMethod.PATCH, "/profile/private/me/update/email").hasRole(USER.name())
+                .antMatchers(HttpMethod.PUT, "/profile/private/me/update").hasRole(USER.name())
+                .antMatchers(HttpMethod.PUT, "/profile/private/me/password").hasRole(USER.name())
+                .antMatchers("/link/{lSig}/vote/direction/{direction}/votecount/{voteCount}").hasRole(USER.name())
+                .antMatchers(HttpMethod.DELETE, "/tag/deleteTag/{tagId}").hasRole(ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/tags/tag/create").hasRole(ADMIN.name())
+                .antMatchers(HttpMethod.POST, "/links/link").hasRole(USER.name())
+                .antMatchers(HttpMethod.POST, "/comments/comment").hasRole(USER.name())
+                .antMatchers("/data/h2-console/**").hasRole(DBA.name())
+                //.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole(ACTUATOR.name())
+                .and()
+                .requiresChannel(requestRegistry -> requestRegistry.anyRequest().requiresSecure())
+                .authorizeRequests(interceptUrlRegistry -> interceptUrlRegistry.anyRequest().permitAll())
+                .formLogin().loginPage("/login")
+                .usernameParameter("email")
+                .successHandler(userSuccessfulAuthenticationHandler())
+                .failureHandler(userFailureAuthenticationHandler())
+                .failureUrl("/login?error")
+                .and()
+                .exceptionHandling().accessDeniedHandler(getAccessDeniedHandler())
+                .and()
+                .logout().deleteCookies("JSESSIONID")
+                .logoutUrl("/logout")
+                .addLogoutHandler(new LogoutHandlerImpl(cacheManager))
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .and()
+                .rememberMe().key("uniqueAndSecret")
+                .tokenValiditySeconds(oneDay)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .maximumSessions(1)
+                .expiredUrl("/login?oneSession");
+    }
 
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) {
-		auth.authenticationProvider(authenticationProvider()).eraseCredentials(false);
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider()).eraseCredentials(false);
+    }
 
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(BeanUtil.getBeanFromContext(BCryptPasswordEncoder.class));
-		provider.setUserDetailsService(getUserDetailsService());
-		return provider;
-	}
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(BeanUtil.getBeanFromContext(BCryptPasswordEncoder.class));
+        provider.setUserDetailsService(getUserDetailsService());
+        return provider;
+    }
 }
