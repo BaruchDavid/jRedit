1395ce54c51e0239814227ed278d9d40c31f9a0a
Baruch David <roman.kaptsan-extern@deutschebahn.com>
2023-04-12 11:15:48 +0200

    fix authetication during test

--- rka-reddit/src/test/java/de/ffm/rka/rkareddit/config/SpringSecurityTestConfig.java
+++ rka-reddit/src/test/java/de/ffm/rka/rkareddit/config/SpringSecurityTestConfig.java
@@ -16,36 +16,42 @@ import java.util.Collections;
 
 /**
  * creates userDetailsService bean for testing
- * @author kaproma
  *
+ * @author kaproma
  */
 @TestConfiguration
-public class SpringSecurityTestConfig   {
-	
-	@Autowired
-	private UserRepository userRepository;
+public class SpringSecurityTestConfig {
 
-	@Autowired
-	private UserDetailsServiceImpl userDetailsService;
-	
-	@SuppressWarnings("unchecked")
-	private Collection<UserDetails> users = Collections.emptyList();
-	
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @SuppressWarnings("unchecked")
+    private Collection<UserDetails> users = Collections.emptyList();
+
     @Bean
     @Primary
     public UserDetailsService userDetailsService() {
-    	
-    	User masterUser = userRepository.findByEmailWithRoles("kaproma@yahoo.de")
-										.orElseThrow(() ->UserDetailsServiceImpl.throwUserNameNotFoundException("kaproma@yahoo.de"));
-    	User dascha = userRepository.findByEmailWithRoles("dascha@gmx.de")
-										.orElseThrow(() ->UserDetailsServiceImpl.throwUserNameNotFoundException("dascha@gmx.de"));
-    	User grom = userRepository.findByEmailWithRoles("grom@gmx.de")
-										.orElseThrow(() ->UserDetailsServiceImpl.throwUserNameNotFoundException("grom@gmx.de"));
-        users = Arrays.asList(masterUser, dascha);
-    	return userDetailsService;
+        //collectUsersFromDatabase();
+        return userDetailsService;
     }
 
-	public Collection<UserDetails> getUsers() {
-		return users;
-	}
+    private void collectUsersFromDatabase() {
+        User masterUser = userRepository.findByEmailWithRoles("kaproma@yahoo.de")
+                .orElse(new User("kaproma@yahoo.de", "$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne"));
+        //.orElseThrow(() ->UserDetailsServiceImpl.throwUserNameNotFoundException("kaproma@yahoo.de"));
+        User dascha = userRepository.findByEmailWithRoles("dascha@gmx.de")
+                .orElse(new User("dascha@gmx.de", "$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne"));
+        //.orElseThrow(() ->UserDetailsServiceImpl.throwUserNameNotFoundException("dascha@gmx.de"));
+        User grom = userRepository.findByEmailWithRoles("grom@gmx.de")
+                .orElse(new User("grom@gmx.de", "$2a$10$kd02GLWJlGR94dyTT7xiLu07CejGocL0oqSSRsInjvwahu3d900ne"));
+        //.orElseThrow(() ->UserDetailsServiceImpl.throwUserNameNotFoundException("grom@gmx.de"));
+        users = Arrays.asList(masterUser, dascha);
+    }
+
+    public Collection<UserDetails> getUsers() {
+        return users;
+    }
 }
--- rka-reddit/src/test/java/de/ffm/rka/rkareddit/controller/TagControllerTest.java
+++ rka-reddit/src/test/java/de/ffm/rka/rkareddit/controller/TagControllerTest.java
@@ -13,84 +13,78 @@ import static org.junit.Assert.assertNotEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class TagControllerTest extends MvcRequestSender{
+public class TagControllerTest extends MvcRequestSender {
 
 
-	/**
-	 * Using Standalone-Configuration, no SpringApplicationContext.
-	 * All additional elements (filter, advices, interceptors) must be set manualy
-	 */
-	@Before
-	public void setup() {
+    /**
+     * Using Standalone-Configuration, no SpringApplicationContext.
+     * All additional elements (filter, advices, interceptors) must be set manualy
+     */
+    @Before
+    public void setup() {
 
-       /* MockitoAnnotations.initMocks(this);
-		this.mockMvc = MockMvcBuilders.standaloneSetup(tagController)
-										.addInterceptors(new ApplicationHandlerInterceptor())
-										.setControllerAdvice(globalControllerAdvice)
-										.setCustomArgumentResolvers(new AuthenticationPrincipalArgumentResolver(), new PageableHandlerMethodArgumentResolver())
-										.build();*/
-	}
-	
-	/**
-	 * @author RKA
-	 * testing new post of valid comment
-	 */
+    }
 
-	@Test
-	@WithUserDetails("kaproma@yahoo.de")
-	public void postNewComment() throws Exception {
+    /**
+     * @author RKA
+     * testing new post of valid comment
+     */
 
-		String body = "java8=";
+    @Test
+    @WithUserDetails("kaproma@yahoo.de")
+    public void postNewComment() throws Exception {
 
-		MvcResult result = super.performPostRequest("/tags/tag/create", body)
-							.andReturn();
-		assertNotEquals("1", result.getResponse().getContentAsString().split(":")[2]);
-	}
+        String body = "java8=";
 
-	@Test
-	@WithUserDetails("kaproma@yahoo.de")
-	public void postAvailibleTag() throws Exception {
-		String body = "java=";
-		MvcResult result = super.performPostRequest("/tags/tag/create", body)
-							.andReturn();
-		assertNotEquals("1", result.getResponse().getContentAsString().split(":")[2]);
-	}
+        MvcResult result = super.performPostRequest("/tags/tag/create", body)
+                .andReturn();
+        assertNotEquals("1", result.getResponse().getContentAsString().split(":")[2]);
+    }
 
-	@Test
-	@WithUserDetails("kaproma@yahoo.de")
-	public void testSchouldNotDeleteRelatedTag() throws Exception {
+    @Test
+    @WithUserDetails("kaproma@yahoo.de")
+    public void postAvailibleTag() throws Exception {
+        String body = "java=";
+        MvcResult result = super.performPostRequest("/tags/tag/create", body)
+                .andReturn();
+        assertNotEquals("1", result.getResponse().getContentAsString().split(":")[2]);
+    }
 
-		MvcResult result = super.mockMvc.perform(MockMvcRequestBuilders.delete("/tags/tag/deleteTag/{tagId}","1")
-																	.secure(true)
-																	.contentType(MediaType.APPLICATION_JSON))
-							.andDo(print())
-							.andExpect(status().isOk())
-							.andReturn();
-		assertEquals("", result.getResponse().getContentAsString());
-	}
+    @Test
+    @WithUserDetails("kaproma@yahoo.de")
+    public void testSchouldNotDeleteRelatedTag() throws Exception {
+
+        MvcResult result = super.mockMvc.perform(MockMvcRequestBuilders.delete("/tags/tag/deleteTag/{tagId}", "1")
+                        .secure(true)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        assertEquals("", result.getResponse().getContentAsString());
+    }
 
 
-	@Test
-	@WithUserDetails("kaproma@yahoo.de")
-	public void testSchouldNotDeleteNotAvailibleTag() throws Exception {
+    @Test
+    @WithUserDetails("kaproma@yahoo.de")
+    public void testSchouldNotDeleteNotAvailibleTag() throws Exception {
 
-		MvcResult result = super.mockMvc.perform(MockMvcRequestBuilders.delete("/tags/tag/deleteTag/{tagId}","101")
-																	.secure(true)
-																	.contentType(MediaType.APPLICATION_JSON))
-							.andExpect(status().isOk())
-							.andReturn();
-		assertEquals("", result.getResponse().getContentAsString());
-	}
-	
-	@Test
-	@WithUserDetails("kaproma@yahoo.de")
-	public void testSchouldDeleteNotRelatedTag() throws Exception {
+        MvcResult result = super.mockMvc.perform(MockMvcRequestBuilders.delete("/tags/tag/deleteTag/{tagId}", "101")
+                        .secure(true)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        assertEquals("", result.getResponse().getContentAsString());
+    }
 
-		MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.delete("/tags/tag/deleteTag/{tagId}","6")
-																	.secure(true)
-																	.contentType(MediaType.APPLICATION_JSON))
-							.andExpect(status().isOk())
-							.andReturn();
-		assertEquals("6", result.getResponse().getContentAsString());
-	}
+    @Test
+    @WithUserDetails("kaproma@yahoo.de")
+    public void testSchouldDeleteNotRelatedTag() throws Exception {
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.delete("/tags/tag/deleteTag/{tagId}", "6")
+                        .secure(true)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+        assertEquals("6", result.getResponse().getContentAsString());
+    }
 }
