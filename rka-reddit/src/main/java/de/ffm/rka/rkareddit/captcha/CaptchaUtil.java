9f507c900980d628b18ae700e0dd27820e12a08e
kaproma <kaproma@yahoo.de>
2023-06-08 09:29:28 +0200

    improve captcha error log

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/captcha/CaptchaUtil.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/captcha/CaptchaUtil.java
@@ -5,11 +5,14 @@ import cn.apiclub.captcha.backgrounds.GradiatedBackgroundProducer;
 import cn.apiclub.captcha.noise.CurvedLineNoiseProducer;
 import cn.apiclub.captcha.text.producer.DefaultTextProducer;
 import cn.apiclub.captcha.text.renderer.DefaultWordRenderer;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.imageio.ImageIO;
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
 
+
+@Slf4j
 public class CaptchaUtil {
 
     public static Captcha createCaptcha(Integer width, Integer height) {
@@ -29,7 +32,7 @@ public class CaptchaUtil {
             byte[] byteArray = Base64.getEncoder().encode(bos.toByteArray());
             image = new String(byteArray);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("ERROR on CAPTCHA {} on Date  {}", captcha.getAnswer(), captcha.getTimeStamp());
         }
         return image;
     }
