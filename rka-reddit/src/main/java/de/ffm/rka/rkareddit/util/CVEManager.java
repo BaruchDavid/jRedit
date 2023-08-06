1b7442eb749f1ef047f57f4f6d85efbb12d4b04e
kaproma <kaproma@yahoo.de>
2023-08-06 10:34:33 +0200

    write critical CVEs

---
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/util/CVEManager.java
@@ -0,0 +1,34 @@
+package de.ffm.rka.rkareddit.util;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class CVEManager {
+    private static Path pathToWrite;
+    public static void main(String[] args) throws IOException {
+
+        pathToWrite = Paths.get("C:\\Skylink\\jRedit\\rka-reddit\\target\\classes\\");
+        String content = IOUtils.toString(Objects.requireNonNull(CVEManager.class.getResourceAsStream("/10.txt")),
+                StandardCharsets.UTF_8);
+        sortForCriticalCVE(content);
+    }
+
+    private static void sortForCriticalCVE(String content) throws IOException {
+        List<String> critical = Arrays.stream(content.split("\n"))
+                .filter(line -> line.contains("Critical"))
+                .collect(Collectors.toList());
+
+        String criticalPath = pathToWrite.toString() + "\\Critical.txt";
+        Path criticalFile = Files.write(Path.of(criticalPath), critical);
+        System.out.printf("path: " + criticalFile);
+    }
+}
