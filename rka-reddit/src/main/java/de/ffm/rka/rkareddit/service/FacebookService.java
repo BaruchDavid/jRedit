7f5eabd9d845fcd6d9586dc0c8b90aa825b73300
Roman Kaptsan <roman.kaptsan@emeal.nttdata.com>
2025-10-19 00:15:03 +0200

    disable fb-dependency

--- rka-reddit/src/main/java/de/ffm/rka/rkareddit/service/FacebookService.java
+++ rka-reddit/src/main/java/de/ffm/rka/rkareddit/service/FacebookService.java
@@ -4,9 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.social.connect.Connection;
-import org.springframework.social.facebook.api.Facebook;
+/* import org.springframework.social.facebook.api.Facebook;
 import org.springframework.social.facebook.api.FacebookLink;
-import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory; */
 import org.springframework.social.oauth2.AccessGrant;
 import org.springframework.social.oauth2.GrantType;
 import org.springframework.social.oauth2.OAuth2Operations;
@@ -25,17 +25,17 @@ public class FacebookService {
 	@Value("${spring.social.facebook.appSecret}")
 	String facebookSecret;
 
-	Facebook facebook;
+	/* Facebook facebook; */
 	String accessToken;
 	private static final Logger LOGGER = LoggerFactory.getLogger(FbController.class);
 
 	public String createFacebookAuthorizationURL() {
-		FacebookConnectionFactory connectionFactory = new FacebookConnectionFactory(facebookAppId, facebookSecret);
-		OAuth2Operations oauthOperations = connectionFactory.getOAuthOperations();
+		//FacebookConnectionFactory connectionFactory = new FacebookConnectionFactory(facebookAppId, facebookSecret);
+		//OAuth2Operations oauthOperations = connectionFactory.getOAuthOperations();
 		OAuth2Parameters params = new OAuth2Parameters();
 		params.setRedirectUri("http://localhost:5550/jReditt/social/facebookAccess");
 		params.setScope("public_profile,email,user_birthday,manage_pages,publish_pages");
-		return oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE,params);
+		return null; //oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE,params);
 	}
 
 	public String getFbId() {
@@ -48,23 +48,23 @@ public class FacebookService {
 
 	public String createFacebookAccessToken(String code) {
 
-		FacebookConnectionFactory connectionFactory = new FacebookConnectionFactory(facebookAppId, facebookSecret);
+		/* FacebookConnectionFactory connectionFactory = new FacebookConnectionFactory(facebookAppId, facebookSecret);
 		AccessGrant accessGrant = connectionFactory.getOAuthOperations().exchangeForAccess(code,
 				"http://localhost:5550/jReditt/social/facebookAccess", null);
 		accessToken = accessGrant.getAccessToken();
 		Connection<Facebook> connection = connectionFactory.createConnection(accessGrant);
         // bind the api
-        facebook = connection.getApi();
+        facebook = connection.getApi(); */
 
 		return accessToken;
 	}
 
 	public String postLinkOnFacebook(Link link, FbPost fbPost) {
-		LOGGER.info("Link {} will be posted on Facebook ", link.toString());
+		/* LOGGER.info("Link {} will be posted on Facebook ", link.toString());
 		FacebookLink fBlink = new FacebookLink("http://www.springsource.org/spring-social", "Spring Social",
 			"The Spring Social Project", "Spring Social is an extension to Spring to enable "
 						+ "applications to connect with service providers.");
-		fbPost.setFbId(facebook.feedOperations().postLink("Awesome", fBlink));
+		fbPost.setFbId(facebook.feedOperations().postLink("Awesome", fBlink)); */
 		
 		return "success";
 	}
